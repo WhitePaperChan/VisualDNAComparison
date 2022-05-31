e6aca751276f18d5d197a9db0c16bd161edcdd5c
WhitePaperChan <Firerosealicorn@gmail.com>
2022-05-31 19:31:56 +0300

    Import

--- main.js
+++ main.js
@@ -6,11 +6,39 @@ let oneVectorLength = 5;
 let DNA1 = 'AAA-TATAATTTTTTATTGACATAAACTGGAAGTTTATGTTAGGATAAGCCAATC';
 let DNA2 = 'AAACTATAATTTTTTATTGACATAAACTTCCAGTTTATGTTAGGATAAGCCAATA';
 
+let importInput1 = document.getElementById('import1');
+let importInput2 = document.getElementById('import2');
+
+let compareButton = document.getElementById('run');
+
+importInput1.addEventListener("change", () => {
+    let file = document.getElementById('import1').files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function() {
+        read_result = reader.result;
+        DNA1 = read_result;
+    };
+});
+
+importInput2.addEventListener("change", () => {
+    let file = document.getElementById('import2').files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function() {
+        read_result = reader.result;
+        DNA2 = read_result;
+    };
+});
+
+compareButton.addEventListener('click', drawComparison);
+
 function partOfPath(x1, y1, x2, y2){
     return 'M ' + (x1 * oneVectorLength) + ', ' + (y1 * oneVectorLength) + '\n L' + (x2 * oneVectorLength) + ', ' + (y2 * oneVectorLength) + '\n';
 }
 
 function drawComparison(){
+    svg.innerHTML = "";
     let path1 = "";
     let path2 = "";
     let x1 = 0;
@@ -71,15 +99,15 @@ function drawComparison(){
     }
     path1 += 'z';
     path2 += 'z';
-    drawPath(path1, 'red');
-    drawPath(path2, 'blue');
+    drawPath(path1, 'red', 0.5);
+    drawPath(path2, 'blue', 1);
 }
 
-function drawPath(path, color){
+function drawPath(path, color, width){
     let element = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     element.setAttribute('d', path);
     element.setAttribute('stroke', color);
-    element.setAttribute('stroke-width', 0.5);
+    element.setAttribute('stroke-width', width);
     element.setAttribute('stroke-linecap', 'round');
     element.setAttribute('vector-effect', 'non-scaling-stroke');
     svg.appendChild(element);
